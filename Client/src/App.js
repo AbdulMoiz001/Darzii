1b9b7df5c0476a447d45cf87d51f2e65937f581c
Abdul Moiz <s.abdulmoiz001@gmail.com>
2023-02-23 14:27:06 +0500

    ClothUi

--- Client/src/App.js
+++ Client/src/App.js
@@ -5,18 +5,20 @@ import StoreScreen from "./components/Screens/Store/StoreScreen";
 import Gateway from "./components/Screens/Signup/Gateway";
 import Settings from "./components/Screens/UserSettings/Settings";
 import "./App.css";
+import ClothUi from "./components/Screens/ClothUi/ClothUi";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       {/* <Settings /> */}
-      <Navbar />
+      {/* <Navbar />
       <Routes>
         <Route path="" element={<Home />} />
         <Route path="Store" element={<StoreScreen />} />
         <Route path="Try" element={<Gateway />} />
-      </Routes>
+      </Routes> */}
+      <ClothUi />
     </div>
   );
 }
