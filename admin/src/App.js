b361ab91800079996c5e0e57743c715395f33379
Abdul Moiz <s.abdulmoiz001@gmail.com>
2023-03-28 17:09:30 +0500

    Admin - Darzii Screens

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-import Admin from './components/Screens/Admin/Admin';
+import Admin from "./components/Screens/Admin/Admin";
+import Darzii from "./components/Screens/Admin/Darzii/Darzii";
+import SignInAdmin from "./components/Screens/SignIn/SignInAdmin";
 
 function App() {
   return (
-    <Admin/>
+    // <Darzii />
+    // <Admin />
+    // <SignInAdmin />
   );
 }
 
