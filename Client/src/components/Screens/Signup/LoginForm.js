3903b61e5dc20315c2110793e7d3dec2e95f4969
Abdul Moiz <s.abdulmoiz001@gmail.com>
2023-04-07 10:14:43 +0500

    localStorage function

--- Client/src/components/Screens/Signup/LoginForm.js
+++ Client/src/components/Screens/Signup/LoginForm.js
@@ -1,65 +1,71 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './Main.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./Main.css";
 
 function LoginForm() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleSubmit = async (event) => {
-        const formData = {
-            email,
-            password
-        };
-        event.preventDefault();
-        try {
-            const res = await axios.post(
-            "http://localhost:5000/auth/loginUser", 
-            formData);
-            const AccessToken = res.data.accessToken;
-        } catch (err) {
-            console.log(err);
-            
-        }
+  const handleSubmit = async (event) => {
+    const formData = {
+      email,
+      password,
     };
-      
+    event.preventDefault();
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/auth/loginUser",
+        formData
+      );
+      //Store user data in LocalStorage
+      localStorage.setItem("user", JSON.stringify(res.data));
+      //
+      //   console.log(JSON.parse(localStorage.getItem("user")));
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-    return (
-        <div className="loginCard">
-            <h2 className="title">Login to Darzii <span className='tagline'>Or instead</span> </h2>
-            <form onSubmit={handleSubmit}>
-                <div className="row">
-                    <div className="col">
-                        <div className="group">
-                            <label className="formLabel" >Email</label>
-                            <input
-                                className="inputStyle"
-                                type="email"
-                                name="email"
-                                value={email}
-                                onChange={(event) => setEmail(event.target.value)}
-                            />
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="group">
-                            <label className="formLabel">Password</label>
-                            <input
-                                className="inputStyle"
-                                type="password"
-                                name="password"
-                                value={password}
-                                onChange={(event) => setPassword(event.target.value)}
-                            />
-                        </div>
-                    </div>
-                </div>
-                <div className="buttonDiv">
-                    <button className="btn-form changeColor" type="submit">Login</button>
-                </div>
-            </form>
+  return (
+    <div className="loginCard">
+      <h2 className="title">
+        Login to Darzii <span className="tagline">Or instead</span>{" "}
+      </h2>
+      <form onSubmit={handleSubmit}>
+        <div className="row">
+          <div className="col">
+            <div className="group">
+              <label className="formLabel">Email</label>
+              <input
+                className="inputStyle"
+                type="email"
+                name="email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
+            </div>
+          </div>
+          <div className="col">
+            <div className="group">
+              <label className="formLabel">Password</label>
+              <input
+                className="inputStyle"
+                type="password"
+                name="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+            </div>
+          </div>
         </div>
-    );
+        <div className="buttonDiv">
+          <button className="btn-form changeColor" type="submit">
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default LoginForm;
