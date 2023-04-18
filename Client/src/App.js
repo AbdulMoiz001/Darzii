b52329537864d850fa6b4501e66aa9c1b19398d3
exprodev <k191115@nu.edu.pk>
2023-04-18 17:24:36 +0500

    new

--- Client/src/App.js
+++ Client/src/App.js
@@ -5,17 +5,13 @@ import StoreScreen from './components/Screens/Store/StoreScreen';
 import Gateway from './components/Screens/Signup/Gateway';
 import ClothUI from './components/Screens/ClothUI/ClothUI';
 import MeasurementForm from './components/Screens/ClothUI/Measurements/MeasurementForm';
-<<<<<<< HEAD
-import Tailor from './components/Screens/Tailor/Tailor';
-import OrderCreation from './components/Screens/Tailor/OrderCreation/OrderCreation';
-=======
 import Blank from './components/Screens/Home/Blank'
->>>>>>> a74fb2f1727549f9517f38e776c25dab100af88a
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { AuthContext, AuthContextProvider } from './context/authContext/AuthContext';
 import SINavbar from './components/Navbar/UserNavbarSignedIn/SignedInNavBar';
-
+import Tailor from './components/Screens/Tailor/Tailor';
+import OrderCreation from './components/Screens/Tailor/OrderCreation/OrderCreation';
 
 
 function App() {
@@ -23,19 +19,6 @@ function App() {
 
   return (
     <div className="App">
-<<<<<<< HEAD
-          <Navbar/>
-          <Routes>
-            <Route path='' element={<Home/>}/>
-            <Route path='Store' element={<StoreScreen/>}/>
-            <Route path='Try' element={<Gateway/>}/>
-            <Route path='ClothUI' element={<ClothUI/>}/>
-            <Route path='MeasurementForm' element={<MeasurementForm/>}/>
-            <Route path='Tailor' element={<Tailor/>}/>
-            <Route path='OrderCreation' element={<OrderCreation/>}/>
-          </Routes>
-    </div>
-=======
       {user ? <SINavbar /> : <Navbar />}
 
       <AuthContextProvider>
@@ -46,10 +29,11 @@ function App() {
           <Route path='/ClothUI' element={<ClothUI />} />
           <Route path='/MeasurementForm' element={<MeasurementForm />} />
           {user ? <Route path='/blank' element={<Blank />} /> : <></>}
+          <Route path='Tailor' element={<Tailor/>}/>
+          <Route path='OrderCreation' element={<OrderCreation/>}/>
         </Routes>
       </AuthContextProvider>
     </div >
->>>>>>> a74fb2f1727549f9517f38e776c25dab100af88a
   );
 }
 
