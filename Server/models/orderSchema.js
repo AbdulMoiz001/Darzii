3fafd3b6bf9fb6fc9eefdad0f73361b7e205bd12
AbdulMoiz001 <78033318+AbdulMoiz001@users.noreply.github.com>
2023-05-17 17:21:07 +0500

    darzi and admin backend func

--- Server/models/orderSchema.js
+++ Server/models/orderSchema.js
@@ -1,81 +1,58 @@
 import mongoose from "mongoose";
 
-const OrderSchema = new mongoose.Schema({
-  user: {
+const orderSchema = new mongoose.Schema({
+  OrderType: { type: String, },
+  ItemID: { type: Number, },
+  ItemTitle: { type: String, },
+  TailorID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "tailor",
+  },
+  TailorName: { type: String, },
+  Size: { type: String, default: null },
+  Measurements: {
+    height: { type: Number, default: null },
+    weight: { type: Number, default: null },
+    chest: { type: Number, default: null },
+    waist: { type: Number, default: null },
+    hips: { type: Number, default: null },
+    shoulder: { type: Number, default: null },
+    sleeves: { type: Number, default: null },
+    neck: { type: Number, default: null }
+  },
+  ClothUI: { type: Boolean, default: false },
+  Design: {
+    beltStyle: { type: String, default: null },
+    cuffsStyle: { type: String, default: null },
+    bottomStyle: { type: String, default: null },
+    trouserStyle: { type: String, default: null },
+    stitchStyle: { type: String, default: null },
+    collarStyle: { type: String, default: null },
+    sleevesStyle: { type: String, default: null },
+    shoulderStyle: { type: String, default: null },
+    neckStyle: { type: String, default: null },
+    lacingStyle: { type: String, default: null }
+  },
+  Catalogue: { type: Boolean, default: true },
+  CatalogueID: { type: Number, default: null },
+  Price: { type: Number, },
+  Title: { type: String, },
+  ClothingType: { type: String, },
+  OrderStatus: { type: String, },
+  CustomerID: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "user",
-    required: true,
   },
-
-  cart: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "orderCart",
-  },
-
-  measurements:
-    { type: mongoose.Schema.Types.ObjectId, ref: "measurementOrder" },
-
-  deliveryAddress:
-  {
-    address_name: {
-      type: String,
-    },
-    city: {
-      type: String,
-    },
-    area: { type: String },
-    address: {
-      type: String,
-    },
-    required: true,
-  },
-  paymentAdress:
-  {
-    address_name: {
-      type: String,
-    },
-    city: {
-      type: String,
-    },
-    area: { type: String },
-    address: {
-      type: String,
-    }
-  },
-
-  is_payed:
-  {
-    type: Boolean,
-    required: true,
-    default: false,
-  },
-
-
-
-  status: {
-    type: String,
-    default: 'Not processed',
-    enum: [
-      'Not processed',
-      'Processing',
-      'Shipped',
-      'Partial Shipped',
-      'Delivered',
-      'Partial Delivered',
-      'Cancelled',
-      'Partial Cancelled',
-    ],
-  },
-
-  updated: {
-    type: Date,
-  },
-  created: {
-    type: Date,
-    default: Date.now,
-  },
+  CustomerContactNumber: { type: String, },
+  CustomerName: { type: String, },
+  TailorContactNumber: { type: String, },
+  OrderAcceptanceDate: { type: Date, },
+  OrderDeliveryDeadline: { type: Date, },
+  PaymentStatus: { type: String, },
+  Rating: { type: Number, }
 });
-const orderSchema = mongoose.model("order", OrderSchema);
 
-export default orderSchema;
+const OrderSchema = mongoose.model("order", orderSchema);
+
+export default OrderSchema;
 
